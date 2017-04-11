da9ea94ec488bd68c63b38699dcda15e2a98389f
clarence hu <clarencehu@Qiandao.local>
2017-04-11 11:10:37 +0800

    first commit

---
+++ server.js
@@ -0,0 +1,25 @@
+const Koa = require('koa');
+const app = new Koa();
+
+// x-response-time
+app.use(async (ctx, next) => {
+  const start = new Date();
+  await next();
+  const ms = new Date() - start;
+  ctx.set('X-Response-Time', `${ms}ms`);
+});
+
+// logger
+app.use(async (ctx, next) => {
+  const start = new Date();
+  await next();
+  const ms = new Date() - start;
+  console.log(`${ctx.method} ${ctx.url} - ${ms}`);
+});
+
+// response
+app.use(ctx => {
+  ctx.body = 'Hello World';
+});
+
+app.listen(3000);
