96ee16724b1864a22ea3c9fe95f2ecc7c8c4a167
clarence hu <clarencehu@Qiandao.local>
2017-04-11 20:45:06 +0800

    add hello-world demo

---
+++ chapter1/hello-world/server.js
@@ -0,0 +1,16 @@
+const Koa = require('koa');
+const Router = require('koa-router');
+
+const app = new Koa();
+const router = new Router();
+
+router.get('/', (ctx, next) => {
+  ctx.body = 'Hello World!';
+});
+
+app
+  .use(router.routes())
+  .use(router.allowedMethods());
+
+app.listen(3000);
+console.log('Koa started on port 3000');
