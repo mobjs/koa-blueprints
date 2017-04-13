ee65fee0ef0318b53661019bcaca89f51dde0c88
clarence hu <clarencehu@Qiandao.local>
2017-04-13 16:23:40 +0800

    add user model

--- chapter1/custom-user-auth/models/user.js
+++ chapter1/custom-user-auth/models/user.js
@@ -1,9 +1,20 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const validator = require('validator');
 
 const UserSchema = new mongoose.Schema({
   email: { type: String, lowercase: true, trim: true, required: true, unique: true },
   password: { type: String },
-  created_at: { type: Date, default: Date.now }
+  created_at: { type: Date, default: Date.now },
+  github: String,
+  tokens: Array,
+  profile: {
+    name: { type: String, default: '' },
+    gender: { type: String, default: '' },
+    location: { type: String, default: '' },
+    website: { type: String, default: '' },
+    picture: { type: String, default: '' }
+  }
 });
 
 UserSchema.pre('save', function(next) {
@@ -14,6 +25,41 @@ UserSchema.pre('save', function(next) {
   next();
 });
 
-const User = mongoose.model('User', UserSchema);
+UserSchema.methods = {
+  validPassword: function(password) {
+    return bcrypt.compareSync(password, this.password);
+  }
+};
+
+UserSchema.statics = {
+  makeSalt: function() {
+    return bcrypt.genSaltSync(10);
+  },
+  encryptPassword: function(password) {
+    if (!password) {
+      return '';
+    }
+    return bcrypt.hashSync(password, User.makeSalt());
+  },
+  register: function(email, password, cb) {
+    var user = new User({
+      email: email,
+      password: password
+    });
+    user.save(function(err) {
+      cb(err, user);
+    });
+  }
+};
+
+var User = mongoose.model('User', UserSchema);
+
+User.schema.path('email').validate(function(email) {
+  return validator.isEmail(email);
+});
+
+User.schema.path('password').validate(function(password) {
+  return validator.isLength(password, 6);
+});
 
 module.exports = User;
